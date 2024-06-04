208a64f235f89de7a3a1baff40e5f4f1a89049ac
Kenny Carlile <kennycarlile@gmail.com>
2024-06-03 23:55:24 -0700

    Adding local linting

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -6,7 +6,14 @@ echo "# Guitar Diagrams JS build script"
 echo "########################################"
 echo ""
 
+LOCAL_PATH=$(dirname "$0")
+
 echo "< Beginning Guitar Diagrams JS build script..."
+echo ""
+
+echo ">> Beginning Super-Linter (local) run..."
+bash -x "$LOCAL_PATH/"super-linter.sh
+echo "<< Ending Super-Linter (local) run."
 echo ""
 
 echo ">> Beginning npm install..."
