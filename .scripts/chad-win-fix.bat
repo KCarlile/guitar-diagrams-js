3adfa3d065b8f20f2653a4451c650335f968d4fb
Kenny Carlile <kennycarlile@gmail.com>
2024-06-04 19:16:16 -0700

    Modifying bat file for Windows fix

--- .scripts/chad-win-fix.bat
+++ .scripts/chad-win-fix.bat
@@ -1,3 +1,5 @@
+echo "Trying to fix Windows symlink issues..."
 mklink ..\docs\js\guitar-diagrams-js\guitar-diagrams.js ..\guitar-diagrams.js
 mklink ..\docs\js\guitar-diagrams-js\guitar-diagrams-config.js ..\guitar-diagrams-config.js
 mklink ..\docs\js\guitar-diagrams-js\guitar-diagrams-marker.js ..\guitar-diagrams-marker.js
+pause
