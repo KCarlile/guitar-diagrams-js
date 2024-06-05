00c9ff3e72ba97427e58f6d4d7f048b0e2083362
Kenny Carlile <kennycarlile@gmail.com>
2024-06-04 19:13:10 -0700

    Adding temporary Windows symlink for Chad

---
+++ .scripts/chad-win-fix.bat
@@ -0,0 +1,3 @@
+mklink ..\docs\js\guitar-diagrams-js\guitar-diagrams.js ..\guitar-diagrams.js
+mklink ..\docs\js\guitar-diagrams-js\guitar-diagrams-config.js ..\guitar-diagrams-config.js
+mklink ..\docs\js\guitar-diagrams-js\guitar-diagrams-marker.js ..\guitar-diagrams-marker.js
