c7ee36b51d2e1683ce3d3cf38b9f7f587f6ae9bd
Kenny Carlile <kennycarlile@gmail.com>
2024-06-11 12:49:42 -0700

    Moving local linting to build-local.sh

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -11,24 +11,6 @@ LOCAL_PATH=$(dirname "$0")
 echo "< Beginning Guitar Diagrams JS build script..."
 echo ""
 
-echo ">> Beginning package checking..."
-source "$LOCAL_PATH/package-check.sh"
-echo "<< Ending package checking."
-echo ""
-
-
-echo ">> Beginning linting..."
-source "$LOCAL_PATH/linting.sh"
-RETURN_CODE=$?
-echo "<< Ending linting."
-echo ""
-
-# If linting fails, it shouldn't reach this point, but just in case...
-if (( RETURN_CODE > 0 )); then
-    echo "[ERROR] Linting failed: see above or run linting.sh with the argument "verbose" for full logs."
-    exit 1
-fi
-
 echo ">> Beginning npm install..."
 npm install ./
 echo "<< Ending npm install."
